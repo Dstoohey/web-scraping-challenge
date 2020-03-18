56933eaec591e868d99d3359f531f3554ca8fbda
Daniel Toohey <dstoohey@gmail.com>
2020-03-18 19:22:32 -0400

    added flask app and DBMS -mongo

--- Mission_to_Mars/Solved/scrape_mars.py
+++ Mission_to_Mars/Solved/scrape_mars.py
@@ -1,49 +1,72 @@
 from splinter import Browser
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup 
 import time
+import pandas as pd
 
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    executable_path = {"executable_path": "chromedriver"}
     return Browser("chrome", **executable_path, headless=False)
 
 
 def scrape_info():
     browser = init_browser()
 
-    # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
+    # mars nasa
+    url = "https://mars.nasa.gov/news/"
     browser.visit(url)
 
     time.sleep(1)
 
     # Scrape page into Soup
     html = browser.html
-    soup = bs(html, "html.parser")
+    soup = BeautifulSoup(html, "html.parser")
+    list_item = soup.select_one("ul.item_list li.slide")
+    content_title = list_item.find("div", class_='content_title').get_text()
+    text_body = list_item.find("div", class_='article_teaser_body').get_text()
+    
+    #Jet Propulsion Lab at CalTech.
 
-    # Get the average temps
-    avg_temps = soup.find('div', id='weather')
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
 
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
+    # mars facts
 
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
+    mars_facts = pd.read_html('https://space-facts.com/mars/')
+    mars_table = mars_facts[0]
+    mars_html_table = mars_table.to_html()
+    mars_html_table = mars_html_table.replace("\n", "")
+    
+    # mars hemispheres
 
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
 
+    hemispheres_url = []
+    links = browser.find_by_css("a.product-item h3")
+
+
+
+
+    
     # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
+    mars_data_dict = {
+        "title": content_title,
+        "body": text_body,
+        "hemispheres": hemispheres_url,
+        "table": mars_html_table
     }
 
     # Close the browser after scraping
     browser.quit()
 
     # Return results
-    return costa_data
+    return mars_data_dict
+
+
+
